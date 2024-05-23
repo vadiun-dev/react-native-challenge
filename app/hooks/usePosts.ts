19c02d5e86a985e11943bc5f4f47adb235d0cd80
Ana Laclau <ana.laclau@globallogic.com>
2024-05-23 08:42:46 -0300

    create services file to fetch data and custom hook to use it

---
+++ app/hooks/usePosts.ts
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+import { getPosts } from "../api/services/Posts";
+
+export const usePosts = () => {
+  const [posts, setPosts] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    loadPosts();
+  }, [page]);
+
+  const loadPosts = async () => {
+    setIsLoading(true);
+    try {
+      const data = await getPosts({ endpoint: "posts", page });
+      setPosts(page === 1 ? data : [...posts, ...data]);
+      setIsLoading(false);
+      setHasMore(data.length === 10);
+    } catch (error) {
+      console.error(error);
+      setError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const refreshPosts = () => {
+    setRefreshing(true);
+    setPage(1);
+    setRefreshing(false);
+  };
+
+  const loadMorePosts = () => {
+    if (hasMore) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  return {
+    posts,
+    refreshing,
+    loadMorePosts,
+    refreshPosts,
+    isLoading,
+    error,
+    loadPosts,
+  };
+};
