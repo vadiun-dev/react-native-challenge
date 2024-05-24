64b9871b77b295633cd8e710d286c20304cc19b2
Ana Laclau <laclauana@gmail.com>
2024-05-23 23:17:11 -0300

    refactor posts list using rtk data fetch, fix sonarlit issues

--- app/(posts)/index.tsx
+++ app/(posts)/index.tsx
@@ -1,49 +1,76 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import {
   ActivityIndicator,
   FlatList,
   RefreshControl,
   SafeAreaView,
 } from "react-native";
-import { usePosts } from "../hooks/usePosts";
 import { Post } from "../types/interfaces";
 import PostCard from "../components/PostCard/PostCard";
 import { top } from "../utils/constants";
 import NotFound from "../components/NotFound/NotFound";
+import { useRouter } from "expo-router";
+import { useLazyGetPostsQuery } from "../state/api";
 
 const initialNumToRender = 10;
 
 const PostsPage = () => {
-  const {
-    posts,
-    refreshing,
-    loadMorePosts,
-    refreshPosts,
-    isLoading,
-    error,
-    loadPosts,
-    router,
-  } = usePosts();
+  const router = useRouter();
+  const [page, setPage] = useState(0);
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [triggerGetPosts, { isLoading, isFetching, isError }] =
+    useLazyGetPostsQuery();
+  const [refreshing, setRefreshing] = useState(false);
+  const [totalPosts, setTotalPosts] = useState<number>();
+
+  useEffect(() => {
+    loadPosts();
+  }, []);
+
+  const loadPosts = useCallback(async () => {
+    if (totalPosts === 0) return;
+    try {
+      const postsResult = await triggerGetPosts(page).unwrap();
+      setPosts((posts) => [...posts, ...postsResult]);
+      setTotalPosts(postsResult.length);
+      setPage((page) => page + 1);
+      setRefreshing(false);
+    } catch (err) {
+      console.error(err);
+    }
+  }, [isFetching, page]);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    loadPosts();
+  };
 
   const onPostPress = (item: Post) => {
     router.push({
-      pathname: `/${item}`,
+      pathname: `/${item.id}`,
       params: {
         ...item,
       },
     });
   };
 
-  const onTryAgainPress = () => loadPosts();
+  const refetchPosts = useCallback(() => {
+    setPage(0);
+    setPosts([]);
+    loadPosts();
+  }, []);
 
   const renderItem = ({ item }: { item: Post }) => (
     <PostCard item={item} onPress={() => onPostPress(item)} />
   );
 
+  const listFooterComponent = () =>
+    isFetching && <ActivityIndicator size={"small"} />;
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      {error && <NotFound tryAgainPress={onTryAgainPress} />}
-      {isLoading && posts.length === 0 ? (
+      {isError && <NotFound tryAgainPress={refetchPosts} />}
+      {isLoading ? (
         <ActivityIndicator size={"large"} />
       ) : (
         <FlatList
@@ -56,19 +83,15 @@ const PostsPage = () => {
           data={posts}
           renderItem={renderItem}
           initialNumToRender={initialNumToRender}
-          keyExtractor={(item) => item.id.toString()}
-          onEndReached={loadMorePosts}
+          keyExtractor={(item, index) => "item" + item.id + "index" + index}
+          onEndReached={loadPosts}
           onEndReachedThreshold={0.3}
           refreshControl={
-            <RefreshControl refreshing={refreshing} onRefresh={refreshPosts} />
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
           }
           refreshing={refreshing}
           showsVerticalScrollIndicator={false}
-          ListFooterComponent={() =>
-            isLoading && posts.length > 0 ? (
-              <ActivityIndicator size={"small"} />
-            ) : null
-          }
+          ListFooterComponent={listFooterComponent}
         />
       )}
     </SafeAreaView>
